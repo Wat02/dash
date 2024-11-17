cf9cf4a1d7a9e7ba99f69ff8cc7a37a3a3d66307
Jovan Jeremic <121092414+Wat02@users.noreply.github.com>
2024-11-17 11:14:30 +0100

    Add files via upload

--- app/(root)/dashboard/layout.tsx
+++ app/(root)/dashboard/layout.tsx
@@ -3,13 +3,13 @@ import Sidebar from "@/components/Siderbar";
 import MobileNav from "@/components/MobileNav";
 import Header from "@/components/Header";
 
-function layout({ childern }: { childern: React.ReactNode }) {
+function layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex h-screen">
       <Sidebar />
       <section className="flex h-screen flex-1 flex-col">
         <MobileNav /> <Header />
-        <div className="main-content">{childern}</div>
+        <div className="main-content">{children}</div>
       </section>
     </main>
   );
